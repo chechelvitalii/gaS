6fa5ece9059dbe1f8f70889884697844bb6d9471
chechelvitalii <vach57vach57vach57>
2017-02-12 02:14:59 +0200

    log4j without yaml

--- src/main/java/com/gas/station/GasStationApplication.java
+++ src/main/java/com/gas/station/GasStationApplication.java
@@ -2,8 +2,11 @@ package com.gas.station;
 
 import com.gas.station.model.Address;
 import com.gas.station.model.GasStation;
+import com.gas.station.model.Service;
 import com.gas.station.model.enums.BrandType;
+import com.gas.station.model.enums.ServiceType;
 import com.gas.station.repository.GasStationRepository;
+import lombok.extern.log4j.Log4j;
 import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
@@ -15,9 +18,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 
+import java.util.Arrays;
 import java.util.List;
 
-@Log4j2
+@Log4j
 @SpringBootApplication
 public class GasStationApplication {
 
@@ -36,7 +40,8 @@ public class GasStationApplication {
             address.setCity("city");
             address.setLat("11");
             address.setLng("22");
-
+//            Service service = new Service(ServiceType.WASHING);
+//            gasStation.setServices(Arrays.asList(service));
             gasStation.setAddress(address);
             log.info("Try save Gas Station");
             GasStation savedGs = gasStationRepository.save(gasStation);
